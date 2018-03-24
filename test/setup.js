e8295605d01cc20dc99367c0070eed1ab5425982
Pierre Cabrière <hello@pierrecabriere.fr>
2018-03-24 19:22:50 +0100

    clean

--- test/setup.js
+++ test/setup.js
@@ -2,8 +2,6 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Authmanager from '../';
 
-// jest.useFakeTimers();
-
 Enzyme.configure({ adapter: new Adapter() });
 
 Authmanager.config.getToken = async (credentials) => {
