abc36039273242bbe0936f77edf3e5484d78d6ed
Pierre Cabrière <hello@pierrecabriere.fr>
2019-02-13 23:50:17 +1300

    fix authmanager

--- authmanager.js
+++ authmanager.js
@@ -15,11 +15,8 @@ var _utils2 = _interopRequireDefault(_utils);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 class _Authmanager {
-  constructor() {
-    this.config = _config2.default;
-    this.utils = _utils2.default;
-  }
-
+  config = _config2.default;
+  utils = _utils2.default;
 }
 
 exports.default = new _Authmanager();
